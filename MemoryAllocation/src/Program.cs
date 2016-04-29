fa0f82d9a04f739cad2073d16a4e6500d75e5e2d
Amr Alaa <n1amr1@gmail.com>
2016-04-29 18:44:38 +0200

    initial commit

---
+++ MemoryAllocation/src/Program.cs
@@ -0,0 +1,23 @@
+﻿using MemoryAllocation.src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryAllocation
+{
+  static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new MainForm());
+    }
+  }
+}
