3897473de1ab4da409181a34242109c45e46422b
Amr Alaa <n1amr1@gmail.com>
2016-04-29 19:38:40 +0200

    add process class

---
+++ MemoryAllocation/src/Process.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryAllocation.src
+{
+  class Process
+  {
+    public String name { get; set; }
+    public int PID { get; set; }
+    public int size { get; set; }
+
+    public Process(String name, int pid, int size)
+    {
+      this.name = name;
+      this.PID = pid;
+      this.size = size;
+    }
+
+    public Process(int size)
+      : this(null, -1, size)
+    {
+    }
+
+    public override String ToString()
+    {
+      return String.Format("Process\tname: {0}\tPID: {1}\tsize: {2}", name, PID, size);
+    }
+  }
+}
