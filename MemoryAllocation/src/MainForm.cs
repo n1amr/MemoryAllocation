c693b3b22a8b29811c833939ba0ee083a5cc93c1
Amr Alaa <n1amr1@gmail.com>
2016-05-07 00:21:28 +0200

    fix dublication in free slots

--- MemoryAllocation/src/MainForm.cs
+++ MemoryAllocation/src/MainForm.cs
@@ -10,157 +10,159 @@ using System.Windows.Forms;
 
 namespace MemoryAllocation
 {
-  public partial class MainForm : Form
-  {
-    private Memory memory;
+	public partial class MainForm : Form
+	{
+		private Memory memory;
 
-    public MainForm()
-    {
-      InitializeComponent();
-    }
+		public MainForm()
+		{
+			InitializeComponent();
+		}
 
-    private void button_Initialize_Click(object sender, EventArgs e)
-    {
-      MemoryInitializationForm form = new MemoryInitializationForm();
-      form.ShowDialog();
+		private void button_Initialize_Click(object sender, EventArgs e)
+		{
+			MemoryInitializationForm form = new MemoryInitializationForm();
+			form.ShowDialog();
 
-      if (form.DialogResult == System.Windows.Forms.DialogResult.OK)
-      {
-        memory = form.getMemory();
-        processCounter++;
-      }
-      refresh();
-    }
+			if (form.DialogResult == System.Windows.Forms.DialogResult.OK)
+			{
+				memory = form.getMemory();
+				processCounter++;
+			}
+			refresh();
+		}
 
-    private void button_allocate_Click(object sender, EventArgs e)
-    {
-      bool successful = memory.allocate(textBox_name.Text, int.Parse(textBox_size.Text));
-      if (!successful)
-        MessageBox.Show("No enough memory space!");
+		private void button_allocate_Click(object sender, EventArgs e)
+		{
+			bool successful = memory.allocate(textBox_name.Text, int.Parse(textBox_size.Text));
+			if (successful)
+			{
+				processCounter++;
+				refresh();
+			}
+			else
+				MessageBox.Show("No enough memory space!");
+		}
 
-      processCounter++;
-      refresh();
-    }
+		private void button_deallocate_Click(object sender, EventArgs e)
+		{
+			int index = listBox_processes.SelectedIndex;
+			if (index >= 0 && index < listBox_processes.Items.Count)
+			{
+				Process process = (Process)listBox_processes.Items[index];
+				memory.deallocate(process);
+				refresh();
+				if (index < listBox_processes.Items.Count)
+					listBox_processes.SelectedIndex = index;
+				else if (listBox_processes.Items.Count != 0)
+					listBox_processes.SelectedIndex = listBox_processes.Items.Count - 1;
+			}
+		}
 
-    private void button_deallocate_Click(object sender, EventArgs e)
-    {
-      int index = listBox_processes.SelectedIndex;
-      if (index >= 0 && index < listBox_processes.Items.Count)
-      {
-        Process process = (Process)listBox_processes.Items[index];
-        memory.deallocate(process);
-        refresh();
-        if (index < listBox_processes.Items.Count)
-          listBox_processes.SelectedIndex = index;
-        else if (listBox_processes.Items.Count != 0)
-          listBox_processes.SelectedIndex = listBox_processes.Items.Count - 1;
-      }
-    }
+		private int processCounter = 0;
+		private void refresh()
+		{
+			listBox_processes.Items.Clear();
+			foreach (Process p in memory.getProcesses())
+				listBox_processes.Items.Add(p);
 
-    private int processCounter = 0;
-    private void refresh()
-    {
-      listBox_processes.Items.Clear();
-      foreach (Process p in memory.getProcesses())
-        listBox_processes.Items.Add(p);
+			textBox_name.Text = "Process " + processCounter.ToString();
+			panel_memory.Refresh();
+		}
 
-      textBox_name.Text = "Process " + processCounter.ToString();
-      panel_memory.Refresh();
-    }
+		private void panel_memory_Paint(object sender, PaintEventArgs e)
+		{
+			base.OnPaint(e);
+			using (Graphics g = e.Graphics)
+			{
+				DrawSlot(g, panel_memory, new MemorySlot(0, 0), memory.getMemorySize(), Color.LightGray);
 
-    private void panel_memory_Paint(object sender, PaintEventArgs e)
-    {
-      base.OnPaint(e);
-      using (Graphics g = e.Graphics)
-      {
-        DrawSlot(g, panel_memory, new MemorySlot(0, 0), memory.getMemorySize(), Color.LightGray);
+				List<MemorySlot> slots = new List<MemorySlot>();
+				slots.AddRange(memory.getFreeSlots());
+				slots.AddRange(memory.getAllocatedSlots());
+				slots.Sort();
+				foreach (MemorySlot slot in slots)
+					DrawSlot(g, panel_memory, slot, memory.getMemorySize(), slot.process == null ? Color.White : Color.LightGray);
+			}
+		}
 
-        List<MemorySlot> slots = new List<MemorySlot>();
-        slots.AddRange(memory.getFreeSlots());
-        slots.AddRange(memory.getAllocatedSlots());
-        slots.Sort();
-        foreach (MemorySlot slot in slots)
-          DrawSlot(g, panel_memory, slot, memory.getMemorySize(), slot.process == null ? Color.White : Color.LightGray);
-      }
-    }
+		private void DrawSlot(Graphics g, Panel panel, MemorySlot slot, int memorySize, Color color)
+		{
+			int startAddress = slot.start;
+			int endAddress = slot.start + slot.size;
+			string slotText = "(Empty)";
+			if (slot.process != null)
+				slotText = slot.process.name;
 
-    private void DrawSlot(Graphics g, Panel panel, MemorySlot slot, int memorySize, Color color)
-    {
-      int startAddress = slot.start;
-      int endAddress = slot.start + slot.size;
-      string slotText = "(Empty)";
-      if (slot.process != null)
-        slotText = slot.process.name;
+			Pen pen = new Pen(Color.Black, 2);
 
-      Pen pen = new Pen(Color.Black, 2);
+			SolidBrush textBrush = new SolidBrush(Color.FromArgb(255 - color.R, 255 - color.G, 255 - color.B));
+			SolidBrush addressBrush = new SolidBrush(Color.Black);
 
-      SolidBrush textBrush = new SolidBrush(Color.FromArgb(255 - color.R, 255 - color.G, 255 - color.B));
-      SolidBrush addressBrush = new SolidBrush(Color.Black);
+			StringFormat sf = new StringFormat();
+			sf.FormatFlags = StringFormatFlags.DirectionRightToLeft;
 
-      StringFormat sf = new StringFormat();
-      sf.FormatFlags = StringFormatFlags.DirectionRightToLeft;
+			int VMargin = 10;
+			int totalHeight = panel.Height - VMargin * 2;
 
-      int VMargin = 10;
-      int totalHeight = panel.Height - VMargin * 2;
+			int slotWidth = panel.Width * 60 / 100;
+			int slotHeight = totalHeight * (endAddress - startAddress) / memorySize;
 
-      int slotWidth = panel.Width * 60 / 100;
-      int slotHeight = totalHeight * (endAddress - startAddress) / memorySize;
+			Point leftTop = new Point(panel.Width * 5 / 100, VMargin + totalHeight * startAddress / memorySize);
+			Point leftBottom = new Point(leftTop.X, leftTop.Y + slotHeight);
+			Point rightBottom = new Point(leftTop.X + slotWidth, leftTop.Y + slotHeight);
+			Point rightTop = new Point(leftTop.X + slotWidth, leftTop.Y);
 
-      Point leftTop = new Point(panel.Width * 5 / 100, VMargin + totalHeight * startAddress / memorySize);
-      Point leftBottom = new Point(leftTop.X, leftTop.Y + slotHeight);
-      Point rightBottom = new Point(leftTop.X + slotWidth, leftTop.Y + slotHeight);
-      Point rightTop = new Point(leftTop.X + slotWidth, leftTop.Y);
+			Point centerPoint = new Point(leftTop.X + slotWidth / 2, leftTop.Y + slotHeight / 2);
+			Point slotTextPosition = centerPoint;
+			Point addressPosition = new Point(rightBottom.X + 10, rightBottom.Y - 10);
 
-      Point centerPoint = new Point(leftTop.X + slotWidth / 2, leftTop.Y + slotHeight / 2);
-      Point slotTextPosition = centerPoint;
-      Point addressPosition = new Point(rightBottom.X + 10, rightBottom.Y - 10);
+			// Draw slot borders
+			g.DrawLine(pen, leftTop, leftBottom);
+			g.DrawLine(pen, rightTop, rightBottom);
+			g.DrawLine(pen, leftTop, rightTop);
+			g.DrawLine(pen, leftBottom, rightBottom);
 
-      // Draw slot borders
-      g.DrawLine(pen, leftTop, leftBottom);
-      g.DrawLine(pen, rightTop, rightBottom);
-      g.DrawLine(pen, leftTop, rightTop);
-      g.DrawLine(pen, leftBottom, rightBottom);
+			// Fill slot background
+			g.FillRectangle(new SolidBrush(color), leftTop.X, leftTop.Y, slotWidth, slotHeight);
 
-      // Fill slot background
-      g.FillRectangle(new SolidBrush(color), leftTop.X, leftTop.Y, slotWidth, slotHeight);
+			// Print process name
+			if (slotHeight > 16)
+			{
+				slotTextPosition.X += slotText.Count() * 6 / 2;
+				slotTextPosition.Y -= 8;
+				g.DrawString(slotText, this.Font, textBrush, slotTextPosition, sf);
+			}
 
-      // Print process name
-      if (slotHeight > 16)
-      {
-        slotTextPosition.X += slotText.Count() * 6 / 2;
-        slotTextPosition.Y -= 8;
-        g.DrawString(slotText, this.Font, textBrush, slotTextPosition, sf);
-      }
+			// Print address label
+			String addressText = "0x" + endAddress.ToString("X");
+			if (slotHeight > 8)
+			{
+				addressPosition.X += addressText.Count() * 6;
+				g.DrawString(addressText, this.Font, addressBrush, addressPosition, sf);
+			}
+		}
 
-      // Print address label
-      String addressText = "0x" + endAddress.ToString("X");
-      if (slotHeight > 8)
-      {
-        addressPosition.X += addressText.Count() * 6;
-        g.DrawString(addressText, this.Font, addressBrush, addressPosition, sf);
-      }
-    }
+		private void MainForm_SizeChanged(object sender, EventArgs e)
+		{
+			int VMargin = 10;
+			panel_memory.Height = this.Height - 6 * VMargin;
+			panel_memory.Top = VMargin;
+			refresh();
+		}
 
-    private void MainForm_SizeChanged(object sender, EventArgs e)
-    {
-      int VMargin = 10;
-      panel_memory.Height = this.Height - 6 * VMargin;
-      panel_memory.Top = VMargin;
-      refresh();
-    }
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			MemoryInitializationForm form = new MemoryInitializationForm();
+			form.ShowDialog();
 
-    private void MainForm_Load(object sender, EventArgs e)
-    {
-      MemoryInitializationForm form = new MemoryInitializationForm();
-      form.ShowDialog();
-
-      if (form.DialogResult == System.Windows.Forms.DialogResult.OK)
-      {
-        memory = form.getMemory();
-        refresh();
-      }
-      else
-        this.Close();
-    }
-  }
+			if (form.DialogResult == System.Windows.Forms.DialogResult.OK)
+			{
+				memory = form.getMemory();
+				refresh();
+			}
+			else
+				this.Close();
+		}
+	}
 }
--- MemoryAllocation/src/Memory.cs
+++ MemoryAllocation/src/Memory.cs
@@ -7,214 +7,225 @@ using System.ComponentModel;
 
 namespace MemoryAllocation
 {
-  public class Memory
-  {
-    public static readonly int FIRST_FIT = 0;
-    public static readonly int BEST_FIT = 1;
-    public static readonly int WORST_FIT = 2;
+	public class Memory
+	{
+		public static readonly int FIRST_FIT = 0;
+		public static readonly int BEST_FIT = 1;
+		public static readonly int WORST_FIT = 2;
 
-    public int algorithm { get; set; }
-    private int memorySize;
+		public int algorithm { get; set; }
+		private int memorySize;
 
-    private List<Process> processes;
-    private List<MemorySlot> allocatedSlots;
-    private List<MemorySlot> freeSlots;
+		private List<Process> processes;
+		private List<MemorySlot> allocatedSlots;
+		private List<MemorySlot> freeSlots;
 
-    public Memory(int algorithm, int memorySize)
-    {
-      List<MemorySlot> free_slots = new List<MemorySlot>();
-      free_slots.Add(new MemorySlot(0, memorySize));
+		public Memory(int algorithm, int memorySize)
+		{
+			List<MemorySlot> free_slots = new List<MemorySlot>();
+			free_slots.Add(new MemorySlot(0, memorySize));
 
-      reset(algorithm, memorySize);
-    }
+			reset(algorithm, memorySize);
+		}
 
-    public void reset(int algorithm, int memorySize)
-    {
-      this.processes = new List<Process>();
-      this.allocatedSlots = new List<MemorySlot>();
-      this.freeSlots = new List<MemorySlot>();
-      this.algorithm = algorithm;
-      this.memorySize = memorySize;
+		public void reset(int algorithm, int memorySize)
+		{
+			this.processes = new List<Process>();
+			this.allocatedSlots = new List<MemorySlot>();
+			this.freeSlots = new List<MemorySlot>();
+			this.algorithm = algorithm;
+			this.memorySize = memorySize;
 
-      addFreeSlot(new MemorySlot(0, memorySize));
-    }
+			addFreeSlot(new MemorySlot(0, memorySize));
+		}
 
-    public bool allocate(String name, int size)
-    {
-      MemorySlot freeSlot = findSlot(size);
-      if (freeSlot != null)
-      {
-        int allocationStart = freeSlot.start;
-        int remainingSize = freeSlot.size - size;
-        int remainingStart = freeSlot.start + size;
+		public bool allocate(String name, int size)
+		{
+			MemorySlot freeSlot = findSlot(size);
+			if (freeSlot != null)
+			{
+				int allocationStart = freeSlot.start;
+				int remainingSize = freeSlot.size - size;
+				int remainingStart = freeSlot.start + size;
 
-        removeSlot(freeSlot, freeSlots);
+				removeSlot(freeSlot, freeSlots);
 
-        if (remainingSize > 0)
-          addFreeSlot(new MemorySlot(remainingStart, remainingSize));
+				if (remainingSize > 0)
+					addFreeSlot(new MemorySlot(remainingStart, remainingSize));
 
-        Process process = new Process(name, size);
-        addSlot(new MemorySlot(allocationStart, size, process), allocatedSlots);
-        processes.Add(process);
+				Process process = new Process(name, size);
+				addSlot(new MemorySlot(allocationStart, size, process), allocatedSlots);
+				processes.Add(process);
 
-        return true;
-      }
-      return false;
-    }
+				return true;
+			}
+			return false;
+		}
 
-    public void deallocate(Process process)
-    {
-      //Process process = processes[index];
-      MemorySlot allocatedSlot = null;
-      foreach (MemorySlot slot in allocatedSlots)
-      {
-        if (slot.process == process)
-        {
-          allocatedSlot = slot;
-          break;
-        }
-      }
+		public void deallocate(Process process)
+		{
+			//Process process = processes[index];
+			MemorySlot allocatedSlot = null;
+			foreach (MemorySlot slot in allocatedSlots)
+			{
+				if (slot.process == process)
+				{
+					allocatedSlot = slot;
+					break;
+				}
+			}
 
-      removeSlot(allocatedSlot, allocatedSlots);
+			removeSlot(allocatedSlot, allocatedSlots);
 
-      MemorySlot freeSlot = new MemorySlot(allocatedSlot.start, allocatedSlot.size);
-      addFreeSlot(freeSlot);
+			MemorySlot freeSlot = new MemorySlot(allocatedSlot.start, allocatedSlot.size);
+			addFreeSlot(freeSlot);
 
-      processes.Remove(process);
-    }
+			processes.Remove(process);
+		}
 
-    public List<Process> getProcesses()
-    {
-      return processes;
-    }
+		public List<Process> getProcesses()
+		{
+			return processes;
+		}
 
-    public List<MemorySlot> getFreeSlots()
-    {
-      return freeSlots;
-    }
+		public List<MemorySlot> getFreeSlots()
+		{
+			return freeSlots;
+		}
 
-    public int getMemorySize()
-    {
-      return memorySize;
-    }
+		public int getMemorySize()
+		{
+			return memorySize;
+		}
 
-    public List<MemorySlot> getAllocatedSlots()
-    {
-      return allocatedSlots;
-    }
+		public List<MemorySlot> getAllocatedSlots()
+		{
+			return allocatedSlots;
+		}
 
-    private MemorySlot findSlot(int size)
-    {
-      MemorySlot slot = null;
-      for (int i = 0; i < freeSlots.Count; i++)
-        if (freeSlots[i].size >= size)
-        {
-          slot = freeSlots[i];
-          break;
-        }
+		private MemorySlot findSlot(int size)
+		{
+			MemorySlot slot = null;
+			for (int i = 0; i < freeSlots.Count; i++)
+				if (freeSlots[i].size >= size)
+					return freeSlots[i];
 
-      return slot;
-    }
+			return null;
+		}
 
-    private void addFreeSlot(MemorySlot slot)
-    {
-      MemorySlot previousSlot = null;
-      MemorySlot nextSlot = null;
+		private MemorySlot mergeSlots(MemorySlot slot1, MemorySlot slot2)
+		{
+			int start1 = slot1.start;
+			int end1 = slot1.start + slot1.size;
 
-      foreach (MemorySlot s in freeSlots)
-      {
-        if (s.start + s.size == slot.start)
-          previousSlot = s;
-        else if (slot.start + slot.size == s.start)
-          nextSlot = s;
-      }
+			int start2 = slot2.start;
+			int end2 = slot2.start + slot2.size;
 
-      if (previousSlot != null)
-      {
-        slot.start = previousSlot.start;
-        slot.size += previousSlot.size;
-        removeSlot(previousSlot, freeSlots);
-      }
-      if (nextSlot != null)
-      {
-        slot.size += nextSlot.size;
-        removeSlot(nextSlot, freeSlots);
-      }
+			if (start1 > start2)
+			{
+				int tmp;
 
-      addSlot(slot, freeSlots);
-    }
+				tmp = start2;
+				start2 = start1;
+				start1 = tmp;
 
-    public void addSlot(MemorySlot slot, List<MemorySlot> slots)
-    {
-      if (slots.Count == 0)
-        slots.Add(slot);
-      else
-      {
-        for (int i = 0; i < slots.Count; i++)
-        {
-          if (algorithm == FIRST_FIT)
-          {
-            if (slot.start < slots[i].start)
-            {
-              slots.Insert(i, slot);
-              return;
-            }
-          }
-          else if (algorithm == BEST_FIT)
-          {
-            if (slot.size < slots[i].size)
-            {
-              slots.Insert(i, slot);
-              return;
-            }
-          }
-          else if (algorithm == WORST_FIT)
-          {
-            if (slot.start > slots[i].start)
-            {
-              slots.Insert(i, slot);
-              return;
-            }
-          }
+				tmp = end1;
+				end1 = end2;
+				end2 = tmp;
+			}
 
-        }
-        slots.Add(slot);
-      }
-    }
+			if (start2 > end1)
+			{
+				return null;
+			}
 
-    public void removeSlot(MemorySlot slot, List<MemorySlot> slots)
-    {
-      slots.Remove(slot);
-    }
+			int start = start1;
+			int end = end1 > end2 ? end1 : end2;
 
-    public void initialize(List<MemorySlot> free_slots)
-    {
-      processes = new List<Process>();
-      freeSlots = new List<MemorySlot>();
-      allocatedSlots = new List<MemorySlot>();
+			return new MemorySlot(start, end - start);
+		}
 
-      free_slots.Sort();
-      int lastAddress = 0;
-      foreach (MemorySlot slot in free_slots)
-      {
-        if (slot.start > lastAddress)
-        {
-          Process process = new Process("Allocated", slot.start - lastAddress);
-          MemorySlot newSlot = new MemorySlot(lastAddress, slot.start - lastAddress, process);
-          addSlot(newSlot, allocatedSlots);
-          processes.Add(process);
-        }
-        addFreeSlot(slot);
-        lastAddress = slot.start + slot.size;
-      }
+		private void addFreeSlot(MemorySlot slot)
+		{
+			addSlot(slot, freeSlots);
 
-      if (memorySize - lastAddress > 0)
-      {
-        Process process = new Process("Allocated", memorySize - lastAddress);
-        MemorySlot newSlot = new MemorySlot(lastAddress, memorySize - lastAddress, process);
-        addSlot(newSlot, allocatedSlots);
-        processes.Add(process);
-      }
-    }
-  }
+			List<MemorySlot> mergingList = new List<MemorySlot>();
+			// Search for mergable slots
+			foreach (MemorySlot s in freeSlots)
+				if (mergeSlots(slot, s) != null)
+					mergingList.Add(s);
+
+			foreach (MemorySlot s in mergingList)
+			{
+				slot = mergeSlots(slot, s);
+				removeSlot(s, freeSlots);
+			}
+
+			addSlot(slot, freeSlots);
+		}
+
+		private bool isBetter(MemorySlot s1, MemorySlot s2, int algorithm)
+		{
+			if (algorithm == FIRST_FIT)
+				return (s1.start < s2.start);
+			else if (algorithm == BEST_FIT)
+				return (s1.size < s2.size);
+			else if (algorithm == WORST_FIT)
+				return (s1.start > s2.start);
+			return false;
+		}
+
+		public void addSlot(MemorySlot slot, List<MemorySlot> slots)
+		{
+			if (slots.Count == 0)
+				slots.Add(slot);
+			else
+			{
+				for (int i = 0; i < slots.Count; i++)
+				{
+					if (isBetter(slot, slots[i], algorithm))
+					{
+						slots.Insert(i, slot);
+						return;
+					}
+				}
+				slots.Add(slot);
+			}
+		}
+
+		public void removeSlot(MemorySlot slot, List<MemorySlot> slots)
+		{
+			slots.Remove(slot);
+		}
+
+		public void initialize(List<MemorySlot> free_slots)
+		{
+			processes = new List<Process>();
+			freeSlots = new List<MemorySlot>();
+			allocatedSlots = new List<MemorySlot>();
+
+			free_slots.Sort();
+			int lastAddress = 0;
+			foreach (MemorySlot slot in free_slots)
+			{
+				if (slot.start > lastAddress)
+				{
+					Process process = new Process("Allocated", slot.start - lastAddress);
+					MemorySlot newSlot = new MemorySlot(lastAddress, slot.start - lastAddress, process);
+					addSlot(newSlot, allocatedSlots);
+					processes.Add(process);
+				}
+				addFreeSlot(slot);
+				lastAddress = slot.start + slot.size;
+			}
+
+			if (memorySize - lastAddress > 0)
+			{
+				Process process = new Process("Allocated", memorySize - lastAddress);
+				MemorySlot newSlot = new MemorySlot(lastAddress, memorySize - lastAddress, process);
+				addSlot(newSlot, allocatedSlots);
+				processes.Add(process);
+			}
+		}
+	}
 }
